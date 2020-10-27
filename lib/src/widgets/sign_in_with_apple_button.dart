8028b6e74071ba56861f7cd9758a57da0e756ab8
Jochen <qiushouchen@qq.com>
2020-10-27 15:19:45 +0800

    Update sign_in_with_apple_button.dart

--- lib/src/widgets/sign_in_with_apple_button.dart
+++ lib/src/widgets/sign_in_with_apple_button.dart
@@ -107,7 +107,7 @@ class SignInWithAppleButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // per Apple's guidelines
-    final fontSize = height * 0.43;
+    final fontSize = 14;
 
     final textWidget = Text(
       text,
